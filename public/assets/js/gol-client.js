a26b3692b96b266b8715e61b4ca4a709782e305c
pnhung177 <pnhung177@gmail.com>
2015-07-23 15:54:27 +0700

    Refactoring gol-client.js source code

--- public/assets/js/gol-client.js
+++ public/assets/js/gol-client.js
@@ -1,4 +1,4 @@
-(function() {
+(function(exports) {
   
   function getBaseURL () {
    return location.protocol + "//" + location.hostname + 
@@ -6,9 +6,108 @@
   }
 
   var GolClient = function() {
-    var self = this;
+    this.socket = io.connect(getBaseURL());
+  };
 
-    self.socket = io.connect(getBaseURL());
+  GolClient.prototype.init = function(params) {
+    var self = this;
+    params = params || {};
+
+    self.selected_cell;
+    self.selected_cells = [];
+    self.collection_mode = false;
+
+    self.canvas = document.getElementById('canvas');
+    self.context = self.canvas.getContext('2d');
+
+    self.width = self.canvas.getAttribute('width');
+    self.height = self.canvas.getAttribute('height');
+
+    self.registerEvent(self.canvas, 'mousedown', function(event) {
+      self.collection_mode = true;
+      self.selected_cell = self.positionOfMouseOnCanvas(event);
+      self.selected_cells.push(self.selected_cell);
+    });
+
+    self.registerEvent(self.canvas, 'mouseup', function(event) {
+      self.socket.emit('request reverse', {cells: self.selected_cells});
+      self.selected_cell = undefined;
+      self.selected_cells = [];
+      self.collection_mode = false;
+    });
+
+    self.registerEvent(self.canvas, 'mousemove', function(event) {
+      if (!self.collection_mode) return;
+      var pos = self.positionOfMouseOnCanvas(event);
+      if (pos.x != self.selected_cell.x || pos.y != self.selected_cell.y) {
+        self.selected_cell = pos;
+        self.selected_cells.push(pos);
+      }
+    });
+
+    self.button_start = document.getElementById('start');
+    self.button_stop = document.getElementById('stop');
+    self.button_step = document.getElementById('step');
+    self.button_reset = document.getElementById('reset');
+    
+    self.button_random = document.getElementById('random');
+    self.checkbox_toroidal = document.getElementById('checkbox_toroidal');
+    self.pattern_list = document.getElementById('pattern-list');
+
+    self.world_cols = document.getElementById('world_cols');
+    self.world_rows = document.getElementById('world_rows');
+    self.status_totalsteps = document.getElementById('totalsteps');
+    self.status_alivecells = document.getElementById('alivecells');
+    self.status_time_usage = document.getElementById('time_usage');
+    self.status_memory_usage = document.getElementById('memory_usage');
+
+    self.registerEvent(self.button_start, 'click', function() {
+      self.socket.emit('start');
+    });
+
+    self.registerEvent(self.button_stop, 'click', function() {
+      self.socket.emit('stop');
+    });
+
+    self.registerEvent(self.button_step, 'click', function() {
+      self.socket.emit('step');
+    });
+
+    self.registerEvent(self.button_reset, 'click', function() {
+      self.socket.emit('reset');
+    });
+
+    self.registerEvent(self.button_random, 'click', function() {
+      self.socket.emit('random');
+    });
+
+    self.registerEvent(self.checkbox_toroidal, 'change', function() {
+      self.socket.emit('request toroidal', {value: self.checkbox_toroidal.checked});
+    });
+
+    self.registerEvent(self.pattern_list, 'change', function() {
+      self.socket.emit('request pattern', {name: self.pattern_list.value});
+    });
+    
+    self.socket.on('response dimension', function(data) {
+      self.cols = data.cols;
+      self.rows = data.rows;
+
+      self.world_cols.innerHTML =data.cols;
+      self.world_rows.innerHTML =data.rows;
+
+      self.cell_border = 1;
+      self.cell_size = 0 - self.cell_border + Math.min(
+        Math.floor((self.width - self.cell_border) / self.cols),
+        Math.floor((self.height - self.cell_border) / self.rows));
+
+      self.drawSpace();
+      self.socket.emit('request init');
+    });
+    
+    self.socket.on('change state', function(data) {
+      self.changeElementState(data.state);
+    });
 
     self.socket.on('change space', function(data) {
       var cells = data.cells || [];
@@ -28,127 +127,8 @@
       mem = parseInt(mem) / 1024;
       self.status_memory_usage.innerHTML = mem;
     });
-  };
 
-  GolClient.prototype.init = function(params) {
-    var self = this;
-
-    self.selected_cell;
-    self.selected_cells = [];
-    self.collection_mode = false;
-
-    params = params || {};
-
-    this.canvas = document.getElementById('canvas');
-    this.context = this.canvas.getContext('2d');
-
-    this.width = this.canvas.getAttribute('width');
-    this.height = this.canvas.getAttribute('height');
-
-    this.registerEvent(this.canvas, 'mousedown', function(event) {
-      self.collection_mode = true;
-      self.selected_cell = self.positionOfMouseOnCanvas(event);
-      self.selected_cells.push(self.selected_cell);
-    });
-
-    this.registerEvent(this.canvas, 'mouseup', function(event) {
-      self.socket.emit('request reverse', {cells: self.selected_cells});
-      self.selected_cell = undefined;
-      self.selected_cells = [];
-      self.collection_mode = false;
-    });
-
-    this.registerEvent(this.canvas, 'mousemove', function(event) {
-      if (!self.collection_mode) return;
-
-      var pos = self.positionOfMouseOnCanvas(event);
-      if (pos.x != self.selected_cell.x || pos.y != self.selected_cell.y) {
-        self.selected_cell = pos;
-        self.selected_cells.push(pos);
-      }
-    });
-
-    self.socket.on('response dimension', function(data) {
-      self.cols = data.cols;
-      self.rows = data.rows;
-
-      self.world_cols.innerHTML =data.cols;
-      self.world_rows.innerHTML =data.rows;
-
-      self.cell_border = 1;
-      self.cell_size = 0 - self.cell_border + Math.min(
-        Math.floor((self.width - self.cell_border) / self.cols),
-        Math.floor((self.height - self.cell_border) / self.rows));
-
-      self.drawSpace();
-      self.socket.emit('request init');
-    });
-
-    this.button_start = document.getElementById('start');
-    this.button_stop = document.getElementById('stop');
-    this.button_step = document.getElementById('step');
-    this.button_reset = document.getElementById('reset');
-    this.button_random = document.getElementById('random');
-    this.checkbox_toroidal = document.getElementById('checkbox_toroidal');
-
-    this.world_cols = document.getElementById('world_cols');
-    this.world_rows = document.getElementById('world_rows');
-    this.status_totalsteps = document.getElementById('totalsteps');
-    this.status_alivecells = document.getElementById('alivecells');
-    this.status_time_usage = document.getElementById('time_usage');
-    this.status_memory_usage = document.getElementById('memory_usage');
-
-    self.pattern_list = document.getElementById('pattern-list');
-
-    this.registerEvent(this.button_start, 'click', function() {
-      self.socket.emit('start');
-    });
-
-    this.registerEvent(this.button_stop, 'click', function() {
-      self.socket.emit('stop');
-    });
-
-    this.registerEvent(this.button_step, 'click', function() {
-      self.socket.emit('step');
-    });
-
-    this.registerEvent(this.button_reset, 'click', function() {
-      self.socket.emit('reset');
-    });
-
-    this.registerEvent(this.button_random, 'click', function() {
-      self.socket.emit('random');
-    });
-
-    self.registerEvent(self.checkbox_toroidal, 'change', function() {
-      self.socket.emit('request toroidal', {value: self.checkbox_toroidal.checked});
-    });
-
-    self.registerEvent(self.pattern_list, 'change', function() {
-      self.socket.emit('request pattern', {name: self.pattern_list.value});
-    });
-    
-    self.socket.on('change state', function(data) {
-      if (data.state === 0) {
-        self.button_start.disabled = false;
-        self.button_stop.disabled = true;
-        self.button_step.disabled = false;
-        self.button_reset.disabled = false;
-        self.button_random.disabled = false;
-        self.checkbox_toroidal.disabled = false;
-        self.pattern_list.disabled = false;
-      } else {
-        self.button_start.disabled = true;
-        self.button_stop.disabled = false;
-        self.button_step.disabled = true;
-        self.button_reset.disabled = true;
-        self.button_random.disabled = true;
-        self.checkbox_toroidal.disabled = true;
-        self.pattern_list.disabled = true;
-      }
-    });
-
-    this.socket.emit('request dimension');
+    self.socket.emit('request dimension');
   }
 
   GolClient.prototype.drawSpace = function() {
@@ -168,6 +148,17 @@
       this.cell_border + (this.cell_border + this.cell_size) * i, 
       this.cell_border + (this.cell_border + this.cell_size) * j, 
       this.cell_size, this.cell_size);
+  }
+
+  GolClient.prototype.changeElementState = function(state) {
+    var stopped = (state === 0);
+    this.button_start.disabled = !stopped;
+    this.button_stop.disabled = stopped;
+    this.button_step.disabled = !stopped;
+    this.button_reset.disabled = !stopped;
+    this.button_random.disabled = !stopped;
+    this.checkbox_toroidal.disabled = !stopped;
+    this.pattern_list.disabled = !stopped;
   }
 
   GolClient.prototype.positionOfMouseOnCanvas = function(event) {
@@ -198,10 +189,12 @@
     ele.addEventListener(event, handler, (capture == true) ? true : false);
   }
 
-  var gol = new GolClient();
+  exports.GolClient = GolClient;
 
-  gol.registerEvent(window, 'load', function() {
-    gol.init();
-  });
+})(this);
 
-})();
+var gol = new GolClient();
+
+gol.registerEvent(window, 'load', function() {
+  gol.init();
+});
